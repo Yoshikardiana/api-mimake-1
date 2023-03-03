30c63a2c7516dbd1bb50102f89022821c99617e0
salsabilazahraandinaaa <andinasalsabillazahra@gmail.com>
2023-03-03 13:54:24 +0700

    first commit

---
+++ app/Http/Controllers/BarangC.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BarangM;
+use Illuminate\Http\Request;
+use App\Http\Resources\BarangR;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class BarangC extends Controller
+{
+    //
+}
